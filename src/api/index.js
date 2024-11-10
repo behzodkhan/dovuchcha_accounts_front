7ada71748acd8795f1880c38d5986e99303be15d
behzodkhan <112095649+behzodkhan@users.noreply.github.com>
2024-11-10 14:12:29 +0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://behzod.pythonanywhere.com',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 api.interceptors.request.use(
